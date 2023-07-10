b3555184db17cbe53daaa239e243df915274c172
V7eGoEGO <Mrnobody@gmail.com>
2023-07-10 17:24:06 +0300

    loader dropDown formLogin

--- dropDown/dropDown.js
+++ dropDown/dropDown.js
@@ -2,22 +2,31 @@ o2.dropDown =
 {
 	open(event)
 	{
-		const dropDown = event.target.closest('.dropDown');
-		const boxHelp = dropDown.querySelector('.dropDown__box');
-		const text = dropDown.querySelectorAll('.dropDown__p');
-		const labelHelper = dropDown.querySelector('.dropDown__input-help');
+		const dropDown = event.target.closest('.dropdown');
+		const boxHelp = dropDown.querySelector('.dropdown__box');
+		const text = dropDown.querySelectorAll('.dropdown__p');
+		const labelHelper = dropDown.querySelector('.dropdown__input-help');
+		const lable = dropDown.querySelector('.dropdown__lable')
+		const dropdownArrow = dropDown.querySelector('.dropdown__arrow');
+		dropdownArrow.classList.remove('dropdown__arrow-up');
+		dropdownArrow.classList.add('dropdown__arrow-down');
+		lable.classList.remove('_hidden');
 		boxHelp.classList.remove('_hidden');
 		labelHelper.classList.add('_hidden');
 		text.forEach(el => el.classList.remove('_hidden'))
-
 	},
 	close(event)
 	{
 
-		const dropDown = event.target.closest('.dropDown');
-		const boxHelp = dropDown.querySelector('.dropDown__box');
-		const labelHelper = dropDown.querySelector('.dropDown__input-help');
+		const dropDown = event.target.closest('.dropdown');
+		const boxHelp = dropDown.querySelector('.dropdown__box');
+		const labelHelper = dropDown.querySelector('.dropdown__input-help');
 		const addOptions = document.createElement('p');
+		const lable = dropDown.querySelector('.dropdown__lable');
+		const dropdownArrow = dropDown.querySelector('.dropdown__arrow');
+		dropdownArrow.classList.add('dropdown__arrow-up');
+		dropdownArrow.classList.remove('dropdown__arrow-down');
+		lable.classList.add('_hidden');
 		boxHelp.classList.add('_hidden');
 		labelHelper.classList.remove('_hidden');
 		if(event.target.value.trim() === "" || event.target.value.trim() === " " )
@@ -28,7 +37,7 @@ o2.dropDown =
 		else
 		{
 			addOptions.textContent = event.target.value;
-			addOptions.classList.add('dropDown__p', '_hidden');
+			addOptions.classList.add('dropdown__p', '_hidden');
 			boxHelp.appendChild(addOptions);
 			event.target.classList.remove('_error');
 			event.target.value = '';
---
+++ logIn/logIn.js
@@ -0,0 +1,13 @@
+o2.logIn =
+{
+	// open()
+	// {
+	// 	const logIn = document.querySelector('.login');
+	// 	logIn.classList.remove('_hidden')
+	// },
+	// closer()
+	// {
+	// 	const logIn = document.querySelector('.login');
+	// 	logIn.classList.add('_hidden')
+	// }
+}
